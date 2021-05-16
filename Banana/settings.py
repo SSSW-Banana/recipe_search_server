5a6f165182c08650c7091ffff4671f0745b7cdc2
p2v2s <squib0612@gmail.com>
2021-05-16 13:35:19 +0900

    add images

--- Banana/settings.py
+++ Banana/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'recipes.apps.RecipesConfig',
+    'images.apps.ImagesConfig',
 
 ]
 
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Banana/urls.py
+++ Banana/urls.py
@@ -13,16 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
+from images.views import ImageViewSet
 from recipes.views import RecipeViewSet
 
+
 router = routers.DefaultRouter()
+router.register('images', ImageViewSet)
 router.register('recipes', RecipeViewSet)
 
 urlpatterns = [
     path(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
